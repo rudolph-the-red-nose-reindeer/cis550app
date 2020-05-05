99638c1e3bcc0a20b655058dc7b2ce7bc157dbdf
rudolph-the-red-nose-reindeer <58148260+rudolph-the-red-nose-reindeer@users.noreply.github.com>
2020-05-05 19:58:11 +0800

    Update index.js

--- webapp/server/index.js
+++ webapp/server/index.js
@@ -13,44 +13,39 @@ app.use(bodyParser.urlencoded({extended: false}));
 /* ------------------- Route handler registration ----------------- */
 /* ---------------------------------------------------------------- */
 
-
-
-
 /* ---- (Dashboard) ---- */
 app.get('/categories', routes.getAllCategories);
-app.get('/categories/:category', routes.getTopProductsInCategory);
-//app.get('/categories/:category', routes.getCheapestReviewedProductsInCategory);
-//app.get('/categories/:category', routes.getMostExpensiveReviewedProductsInCategory);
-//app.get('/categories/:category', routes.getMostReviewedProductsInCategory);
+app.get('/categories/:category', routes.getTopReviewedProductsInCategory);
+app.get('/categories/:category', routes.getCheapestReviewedProductsInCategory);
+app.get('/categories/:category', routes.getMostExpensiveReviewedProductsInCategory);
+app.get('/categories/:category', routes.getMostReviewedProductsInCategory);
+
+
 /* ---- (Reviews) ---- */
-//app.get('/reviews', routes.getLongestReviews);
+app.get('/reviews', routes.getTopreviews);
+app.get('/reviews', routes.getNewestReviews);
 
 /* ---- (Products) ---- */
+app.get('/products', routes.getUnreviewedProducts);//NEW MUST INCLUDE UNVERSAL QUANTIFIER QUERY
+app.get('/products', routes.getTopProductReviews); //NEW
 app.get('/products/:product', routes.getProductInfo);
 app.get('/Product%20stats/:product', routes.getProductStats);
+app.get('/products', routes.getNewestProductReviews); //NEW
+app.get('/Get%20product%20labels', routes.getRelations);
+app.get('/Get%20related%20products/:title/:relation', routes.getRelated);
+
+/* ---- (Brands) ---- */
 app.get('/Top%20products%20in%20a%20brand/:brand', routes.getTopProductsInBrand);
 app.get('/Most%20reviewed%20brands/', routes.getMostReviewedBrands);
 app.get('/Most%20expensive%20products%20per%20brand/:brand', routes.getMostExpensiveProductsInBrand);
 app.get('/Average%20rating%20and%20price%20in%20a%20brand/:brand', routes.getBrandStats);
-app.get('/Get%20product%20labels', routes.getRelations);
-app.get('/Get%20related%20products/:title/:relation', routes.getRelated);
+
 
 /* ---- (Reviewers) ---- */
-//app.get('/reviewers', routes.getTopReviewers);
-app.get('/reviewers/:reviewer', routes.getReviewerStats);
 app.get('/Get%20top%20reviewers', routes.getTopReviewers);
-app.get('/time%20spent%20writing/:reviewer', routes.getReviewerTime);
+app.get('/reviewers/:reviewer', routes.getReviewerStats);
+app.get('/reviewers/:reviewer', routes.getNewestReviewsByReviewer); //NEW
 app.get('/Top%20products%20a%20reviewer%20has%20reviewed/:reviewer', routes.getTopReviewsByReviewer);
-
-/* ---- (Reviews) ---- */
-app.get('/reviews/:title', routes.getLongestReviews);
-//app.get('/reviews', routes.getNewestReviews);
-
-app.get('/products/:info', routes.getProductInfo);
-//need to change reccomendations... to productinfo
-
-
-
 
 app.listen(8081, () => {
 	console.log(`Server listening on PORT 8081`);
